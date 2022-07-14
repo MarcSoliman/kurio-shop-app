f573af9c13b4ad5983475c3ded8cd5acffe1cf76
Marc Soliman <71621832+MarcSoliman@users.noreply.github.com>
2022-07-14 13:06:14 -0500

    Started working with mongoose

--- client/src/components/productpage/productpagebody/rightpanelproducts/RightPanelProducts.tsx
+++ client/src/components/productpage/productpagebody/rightpanelproducts/RightPanelProducts.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import axios from "axios";
 import "./rightpanelproducts.scss";
 function RightPanelProducts() {
-  return <section className="right-panel">RightPanelProducts</section>;
+  return (
+    <section className="right-panel">
+      RightPanelProducts
+      <button
+        onClick={() =>
+          axios.post("http://localhost:5000/api", {
+            name: "Cool Mist",
+          })
+        }
+      >
+        CLICK TO ADD PRODUCT
+      </button>
+    </section>
+  );
 }
 
 export default RightPanelProducts;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,64 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/kurioDB");
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/kurioDB", {
+  useNewUrlParser: true,
+});
+
+const productsSchema = new mongoose.Schema({
+  _id: { type: Number, required: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+});
+
+const Product = mongoose.model("Product", productsSchema);
+
+// const product = new Product({
+//   _id: 1,
+//   name: "Facial Lotion",
+//   description: "Look confident, look like you.",
+//   rating: 5,
+// });
+
+// product.save();
+let productNames = [];
+
+Product.find((err, products) => {
+  if (err) {
+    console.log(err);
+  } else {
+    products.forEach((product) => {
+      productNames.push(product.name);
+    });
+  }
+});
 
 app.get("/api", (req, res) => {
-  res.json({ users: ["userOne", "userTwo", "userThree", "userFour"] });
+  res.json({ products: productNames });
 });
+
+// app.post("/api", (req, res) => {                   //testing post requests to mongodb
+//   const productName = req.body.name;
+//   const product = new Product({
+//     _id: 3,
+//     name: productName,
+//     description: "A custom product",
+//     rating: 2,
+//   });
+//   product.save();
+//   console.log(productName);
+//   // mongoose.connection.close();
+// });
 
 app.listen(5000, () => {
   console.log("Server started on port 5000");
